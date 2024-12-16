9a39beacda86bfeb4e4f41780cd46d839c38e09b
Letice Zwinger <leticezwinger@Letices-Air.lan>
2024-12-16 13:02:40 -0600

    remove error code

--- src/components/EditProfileModal/EditProfileModal.jsx
+++ src/components/EditProfileModal/EditProfileModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import  { useState, useContext, useEffect } from "react";
 import { CurrentUserContext } from "../../Contexts/CurrentUserContext";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import "./EditProfileModal.css";
