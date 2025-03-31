ca7e7c1058843157f173f1d81ff743ee4d59f3d3
Letice Zwinger <leticedss@gmail.com>
2025-03-31 14:31:54 -0500

    revert

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/se_project_react/",
+  base: "/",
   plugins: [react()],
 });
